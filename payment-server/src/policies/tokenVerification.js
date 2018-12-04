24017c80c58c710a25faa24f10c047c5ce734651
darekaze <darekaze@localhost.localdomain>
2018-12-05 00:28:33 +0800

    Refactor payment server code

--- payment-server/src/policies/tokenVerification.js
+++ payment-server/src/policies/tokenVerification.js
@@ -1,1 +1,3 @@
 const jwt = require('jsonwebtoken'); // use for verify token
+
+// TODO: Verify token here
--- payment-server/src/routes.js
+++ payment-server/src/routes.js
@@ -1,19 +1,17 @@
-const ChargingControl = require('./controllers/ChargingController');
 const PurchaseController = require('./controllers/PurchaseController');
-// const isAuthenticated = require('./rules/isAuth');
-
+const tokenVerification = require('./policies/tokenVerification');
 
 const testConnect = (req, res) => {
   console.log('Testing api');
   res.send({ test: true });
 };
 
-
 module.exports = (app) => {
   app.get('/test', testConnect);
 
-  app.post('/purchase', PurchaseController.purchase);
-
+  app.post('/purchase',
+    PurchaseController.purchase);
   app.post('/charging',
-    ChargingControl.charging); // TODO: add verify middleware
+    // tokenVerification,
+    PurchaseController.charging);
 };
