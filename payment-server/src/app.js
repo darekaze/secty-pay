e1d2c3bf800da746db3a64a63d6686d9c5b06733
reynoldpolyu <38733045+reynoldpolyu@users.noreply.github.com>
2018-12-05 01:53:43 +0800

    Import and used express-rate-limit

--- payment-server/src/app.js
+++ payment-server/src/app.js
@@ -2,10 +2,21 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
 const morgan = require('morgan');
+const rateLimit = require('express-rate-limit');
 const config = require('./config');
 
 const app = express();
 
+
+app.enable('trust proxy'); // only if you're behind a reverse proxy (Heroku, Bluemix, AWS ELB, Nginx, etc)
+
+const limiter = rateLimit({
+  windowMs: 15 * 60 * 1000, // 15 minutes
+  max: 5, // limit each IP to 100 requests per windowMs
+});
+
+//  apply to all requests
+app.use(limiter);
 app.use(morgan('combined'));
 app.use(bodyParser.json());
 app.use(cors());
