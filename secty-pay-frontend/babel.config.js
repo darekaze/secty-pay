31e1e4ef8b6571ee698dc5c7c922a38803b80601
DaRekaze <darekaze@gmail.com>
2018-12-06 01:13:42 +0800

    Change back to normal

--- secty-pay-frontend/babel.config.js
+++ secty-pay-frontend/babel.config.js
@@ -1,22 +1,3 @@
 module.exports = {
-  presets: [
-    [
-      '@vue/app',
-      {
-        useBuiltIns: 'entry',
-      },
-    ],
-  ],
-  plugins: [
-    [
-      'transform-imports',
-      {
-        vuetify: {
-          // eslint-disable-next-line no-template-curly-in-string
-          transform: 'vuetify/es5/components/${member}',
-          preventFullImport: true,
-        },
-      },
-    ],
-  ],
+  presets: ['@vue/app'],
 };
